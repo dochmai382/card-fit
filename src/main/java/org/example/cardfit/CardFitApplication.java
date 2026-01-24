5ec64b47ffd667a0cc194aa7709d2c368737b4d2
dochmai382 <dochmai382@gmail.com>
2026-01-24 14:59:37 +0900

    chore: 프로젝트 초기 구조 및 Security/DB 설정 완료

---
+++ src/main/java/org/example/cardfit/CardFitApplication.java
@@ -0,0 +1,13 @@
+package org.example.cardfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardFitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cardfit/CardFitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cardfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
